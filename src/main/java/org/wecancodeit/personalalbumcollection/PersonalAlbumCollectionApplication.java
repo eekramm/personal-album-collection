ddd864ac63901dedba77a5b3c092f88d1e0d7ed6
Eythan Kramm <kramm.eythan@gmail.com>
2019-03-18 20:51:25 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/personalalbumcollection/PersonalAlbumCollectionApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.personalalbumcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAlbumCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAlbumCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/personalalbumcollection/PersonalAlbumCollectionApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.personalalbumcollection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalAlbumCollectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
